d86042f177d9538e0f93d2ac8ecd89ff7e9ff204
devdiegodv <donosoveradiego@gmail.com>
2025-01-06 03:21:28 -0300

    se carga llamada a archivo .env

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-const app = require('./server')
+require('dotenv').config();
+const app = require('./server');
+require('./database');
+
 
 app.listen(app.get('port'), () => {
     console.log("Server open ", app.get('port'))
