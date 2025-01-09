eaccc198c87b1eec32bcff14681998240ea92254
devdiegodv <donosoveradiego@gmail.com>
2025-01-08 23:57:05 -0300

    se agregan rutas

--- src/server.js
+++ src/server.js
@@ -40,9 +40,7 @@ app.use(express.urlencoded({extended: false}));
  */
 
 // Routes
-app.get('/', (req, res) => {
-    res.render('index');
-})
+app.use(require('./routes/index.routes'))
 
 // Static files
 app.use(express.static(path.join(__dirname, '/public')));
